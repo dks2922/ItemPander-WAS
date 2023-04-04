d62f229615c4249c4f91a82092e5c77981638db8
top <dks2922@gmail.com>
2023-04-04 23:19:27 +0900

    remove : jwt 유효성, 토큰발급 추가

---
+++ src/main/java/util/jwt/JwtTokenProvider.java
@@ -0,0 +1,48 @@
+package util.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtTokenProvider {
+
+    private static final String JWT_SECRET = "7858869558953243";
+
+    private static final int EXPIRATION_TIME = 86400000;
+
+    public static String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", username);
+
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + EXPIRATION_TIME);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(SignatureAlgorithm.HS256, JWT_SECRET)
+                .compact();
+    }
+
+    public static String getUsernameFromToken(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.get("username", String.class);
+    }
+
+    public static boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(JWT_SECRET).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
